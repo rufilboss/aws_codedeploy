57750795b679fb04f2a8a9ad44943d7485bf0364
rufilboy <rufaiilyas600@gmail.com>
2021-12-17 17:50:06 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hello Everyone, My name is Ilyas Rufai.</h1>
+    <h1>Hello Everyone, My name is Ilyas Rufai</h1>
     <h2>Aspiring DevOps Engineer...</h2>
     <p>Welcome To My First And New Gatsby Website.</p>
     <p>This Is Just Test A Website.</p>
